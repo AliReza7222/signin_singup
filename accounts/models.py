5299822d9eea9515c340f5c4a6da3ba713fe4b4f
AliReza7222 <ali.r.salehi99@gmail.com>
2023-05-05 00:37:36 +0430

    field first_name and last_name and validators for fields add

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .validators import check_phone_number, check_last_and_first_name
+
 
 # custom model user .
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=11)
+    first_name = models.CharField(max_length=150, validators=[check_last_and_first_name])
+    last_name = models.CharField(max_length=150, validators=[check_last_and_first_name])
+    phone_number = models.CharField(max_length=11, unique=True, validators=[check_phone_number])
 
